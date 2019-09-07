d96ca5dfdff47aa61d1872c8e20d77f533c2b5f2
Samim Qeyam <44156539+qeyamsamim@users.noreply.github.com>
2019-09-07 22:47:09 +0600

    Add files via upload

---
+++ 1003-Simple Sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int sum = a + b;
+		
+		System.out.println("SOMA = " + sum);
+	}
+}
