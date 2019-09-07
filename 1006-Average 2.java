5d91a54c47009094e14c6f3da15a678ea96138ea
Samim Qeyam <44156539+qeyamsamim@users.noreply.github.com>
2019-09-07 22:56:12 +0600

    Add files via upload

---
+++ 1006-Average 2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		
+		double a = scan.nextDouble();
+		double b = scan.nextDouble();
+		double c = scan.nextDouble();
+		
+		double average = ((a*2) + (b*3) + (c*5))/10;
+		
+		System.out.printf("MEDIA = %.1f\n", average);
+	}
+}
