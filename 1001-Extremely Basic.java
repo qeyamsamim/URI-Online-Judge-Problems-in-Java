508f52af81cf3910cb5a21ec2175caa8a9ec61cb
samimqeyam <qeyam_a@auca.kg>
2019-09-07 22:32:50 +0600

    First commit

---
+++ 1001-Extremely Basic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		
+		int fn = scan.nextInt();
+		int sn = scan.nextInt();
+		
+		int answer = fn + sn;
+		System.out.println("X = " + answer);
+	}
+}
