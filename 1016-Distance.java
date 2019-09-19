624d3b6f9eb43156562d33b2a8d83ae9c8020bc7
Samim Qeyam <44156539+qeyamsamim@users.noreply.github.com>
2019-09-19 20:57:05 +0600

    Add files via upload

---
+++ 1016-Distance.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		int distance = scan.nextInt();
+		int time = distance * 2;
+		
+		System.out.println(time + " minutos");
+	}
+}
