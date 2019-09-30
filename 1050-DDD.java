0c855dc329973c3b66a623f01b9f73e478b9311d
Samim Qeyam <44156539+qeyamsamim@users.noreply.github.com>
2019-09-30 19:31:37 +0600

    Add files via upload

---
+++ 1050-DDD.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		if(a == 61){
+			System.out.println("Brasilia");
+		}
+		else if(a == 71){
+			System.out.println("Salvador");
+		}
+		else if(a == 11){
+			System.out.println("Sao Paulo");
+		}
+		else if(a == 21){
+			System.out.println("Rio de Janeiro");
+		}
+		else if(a == 32){
+			System.out.println("Juiz de Fora");
+		}
+		else if(a == 19){
+			System.out.println("Campinas");
+		}
+		else if(a == 27){
+			System.out.println("Vitoria");
+		}
+		else if(a == 31){
+			System.out.println("Belo Horizonte");
+		}
+		else{
+			System.out.println("DDD nao cadastrado");
+		}
+	}
+}
